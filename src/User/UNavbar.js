1ace65d43d38fd2315e29e9461195c4ec52dff87
davemugisha <davidlivingstonemug@gmail.com>
2023-12-18 09:37:51 +0200

    Logout

--- src/User/UNavbar.js
+++ src/User/UNavbar.js
@@ -8,7 +8,7 @@ const UNavbar = () => {
                 <ul className='UserMenu'>
                     <li style={{ float: 'left', }}><img src={Logo} width="50px" /></li>
                     <li style={{ float: 'left', }}><a>My Art Gallery</a></li>
-                    <li><Link to="/User/Logout">Logout</Link></li>
+                    <li><Link to="/Login">Logout</Link></li>
                     <li><Link to="/User/Profile">Profile</Link></li>
                     <li><Link to="/User/Upload">Upload</Link></li>
                     <li><Link to="/User/UTrending">Trending</Link></li>
